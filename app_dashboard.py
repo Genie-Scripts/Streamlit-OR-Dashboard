806bac8702830a58cb9a462d079fd5c1d0b9e508
Genie-Scripts <genie.mobile.phone@gmail.com>
2025-06-06 22:01:32 +0900

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -134,7 +134,7 @@ try:
     from analyzer import analyze_hospital_summary, analyze_department_summary, calculate_recent_averages, filter_data_by_period # ← この行のコメントを解除
     from monthly_quarterly_analyzer import analyze_monthly_summary, analyze_quarterly_summary
     from target_loader import load_target_file
-    # from plotter import plot_summary_graph, plot_department_graph
+    from plotter import plot_summary_graph, plot_department_graph
     # from department_ranking import calculate_department_achievement_rates, plot_achievement_ranking
     # from surgeon_analyzer import create_surgeon_analysis
     # from prediction_tab_enhanced import create_prediction_tab
